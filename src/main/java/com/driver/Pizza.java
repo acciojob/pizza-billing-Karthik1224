45893bdf2273f18bee96b7fdcf8e49cb87c38b8f
Karthik1224 <durgamkarthik150@gmail.com>
2023-07-13 10:16:04 +0530

    updated code

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,31 +4,61 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private Integer bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+         if(isVeg == true)
+         {
+             price = 300;
+             bill = 300;
+         }
+         else
+         {
+             price = 400;
+             bill = 400;
+         }
     }
 
-    public int getPrice(){
+    public int getPrice()
+    {
         return this.price;
+
     }
 
     public void addExtraCheese(){
         // your code goes here
+          if(isVeg==true)
+          {
+              bill += 80;
+          }
+          else
+          {
+              bill += 80;
+          }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg==true)
+        {
+            bill += 70;
+        }
+        else
+        {
+            bill += 120;
+        }
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        bill += 20;
     }
 
     public String getBill(){
         // your code goes here
-        return this.bill;
+        return String.valueOf(bill);
     }
 }
