0f330b3a94e05421f492d70f10f5d7877589a959
Karthik1224 <durgamkarthik150@gmail.com>
2023-07-13 10:51:13 +0530

    changes made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,16 +3,16 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
+    System.out.println("Base Price of The Pizza: "+dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
-    System.out.println(dp.getBill());
+    System.out.println("Total Price: "+dp.getBill());
 
     Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
     p.addTakeaway();
-    System.out.println(p.getBill());
+    System.out.println("Total Price: "+p.getBill());
   }
 }
